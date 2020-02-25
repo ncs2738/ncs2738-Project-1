afb88dba149d25668cf6ec598f9851e3e6729484
ncs2738 <ncs2738@rit.edu>
2020-02-25 02:25:03 -0500

    Got the prototype working

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,4 +1,6 @@
-let numerics;
+const fs = require('fs');
+
+// let numerics;
 let dates;
 
 const respondJSON = (request, response, status, object) => {
@@ -7,145 +9,51 @@ const respondJSON = (request, response, status, object) => {
   response.end();
 };
 
-const respondJSONMeta = (request, response, status) => {
-  response.writeHead(status, { 'Content-Type': 'application/json' });
-  response.end();
-};
+const getDate = (request, response) => {
+  const today = new Date();
+  const d = today.getDate();
+  const m = today.getMonth();
+  // const s = Math.ceil(m / 4);
+  const y = today.getFullYear();
+  const w = today.getDay();
 
-const getUsers = (request, response, requestMethod) => {
-  const responseJSON = dates;
+  const day = dates.days[d];
+  const weekday = dates.weekdays[w];
+  const month = dates.months[m];
 
-  //if (requestMethod === 'GET') responseJSON.users = users;
+
+  const responseJSON = {
+    date: `${m + 1}/${d + 1}/${y}`,
+    kanji: `${month.kanji + day.kanji} (${weekday.kanji})`,
+    reading: `${month.reading} ${day.reading} (${weekday.reading})`,
+    english: `${month.english} ${day.english} (${weekday.english})`,
+    translation: `${month.month} ${day.date} (${weekday.day})`,
+  };
+
+  // if (requestMethod === 'GET') responseJSON.users = users;
 
   respondJSON(request, response, 200, responseJSON);
 };
 
-/*
-const notFound = (request, response, requestMethod) => {
-  const responseJSON = {
-    id: 'notFound',
-  };
-
-  if (requestMethod === 'GET') responseJSON.message = 'The page you are looking for was not found.';
-
-  respondJSON(request, response, 404, responseJSON);
-};
-*/
-
-const notFound = (request, response) =>
-{
-	const statusCode = 404;
-	response.writeHead(statusCode, {'Content-Type' : 'text/html'});
-	response.write("<html><body><h1>404 - Not Found</h1></body></html>");
-	response.end();
-};
-
-
-const addUser = (request, response, body) => {
-  const responseJSON = {
-    message: 'Name and age are both required.',
-  };
-
-  if (!body.name || !body.age) {
-    responseJSON.id = 'missingParams';
-    return respondJSON(request, response, 400, responseJSON);
-  }
-
-  let responseCode = 201;
-
-  if (users[body.name]) {
-    responseCode = 204;
-  } else {
-    users[body.name] = {};
-  }
-
-  users[body.name].name = body.name;
-  users[body.name].age = body.age;
-
-  if (responseCode === 201) {
-    responseJSON.message = 'Created Successfully';
-    return respondJSON(request, response, responseCode, responseJSON);
-  }
-  return respondJSONMeta(request, response, responseCode);
+const notFound = (request, response) => {
+  const statusCode = 404;
+  response.writeHead(statusCode, { 'Content-Type': 'text/html' });
+  response.write('<html><body><h1>404 - Not Found</h1></body></html>');
+  response.end();
 };
 
 module.exports = {
-  getUsers,
-  addUser,
+  getDate,
   notFound,
 };
 
-/*
-Sunday: Nichiyôbi (にちようび – 日曜日)
-Monday: Getsuyôbi (げつようび – 月曜日)
-Tuesday: Kayôbi (かようび – 火曜日)
-Wednesday: Suiyôbi (すいようび – 水曜日)
-Thursday: Mokuyôbi (もくようび – 木曜日)
-Friday: Kinyôbi (きんようび – 金曜日)
-Saturday: Doyôbi (どようび – 土曜日)
-*/
-
-let init = () => 
-{
+const init = () => {
   // load in the JSON data
-  const numberKanji = fs.readFileSync(`${__dirname}../json/Numerical-Kanji.json`);
-  const dateFile = fs.readFileSync(`${__dirname}../json/Dates.json`);
-  
-  numerics = JSON.parse(numberKanji).numbers;
-  dates = JSON.parse(dateFile).numbers;
-	//console.log(`There are ${jokesArray.length} jokes!`);
-}
+  // const numberKanji = fs.readFileSync(`${__dirname}/../json/Numerical-Kanji.json`);
+  const dateFile = fs.readFileSync(`${__dirname}/../json/Dates.json`);
+
+  // numerics = JSON.parse(numberKanji).numbers;
+  dates = JSON.parse(dateFile).dates;
+};
 
 init();
-
-/*
-
-// request handlers
-const handlerGetAllJokes= (request, response) =>
-{
-	const statusCode = 200;												//TURN ON CORS
-	response.writeHead(statusCode, {'Content-Type' : 'application/json', 'Access-Control-Allow-Origin' : '*'});
-	response.write(JSON.stringify(jokesArray));
-	response.end();
-};
-
-const handlerGetRandomJoke = (request, response) =>
-{
-	let randJoke = jokesArray[Math.floor(Math.random() * jokesArray.length)];
-	const statusCode = 200;												//TURN ON CORS
-	response.writeHead(statusCode, {'Content-Type' : 'application/json', 'Access-Control-Allow-Origin' : '*'});
-	response.write(JSON.stringify(randJoke));
-	response.end();
-};
-
-//function to add a joke from a POST'ed joke
-const handlerAddJoke = (request, response, jokes) => 
-{
-	const responseJSON = {
-	  message: 'question and answer are both required.',
-	};
-
-	if (!jokes.q|| !jokes.a) {
-		responseJSON.id = 'missingParams';
-		response.writeHead(400, {'Content-Type' : 'text/html'});
-		response.write("<html><body><h1>400 - question and answer are both required</h1></body></html>");
-	}
-  
-	let responseCode = 201;
-  
-	if (jokesArray[jokes.q]) {
-	  responseCode = 204;
-	} else {
-	  jokesArray[jokes.q] = {};
-	}
-
-	jokesArray[jokes.q].q= jokes.q;
-	jokesArray[jokes.q].age = jokes.a;
-  
-	if (responseCode === 201) {
-	  responseJSON.message = 'Created Successfully';
-	  response.write("<html><body><h1>201 - new joke written!</h1></body></html>");
-	}
-  };
-
-*/
